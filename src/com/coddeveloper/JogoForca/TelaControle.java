a3d300214671720ea6d2cbfc19f61507fccc34fa
FreeDeveloper <developer@freedeveloper.com.br>
2017-06-17 15:05:26 -0300

    Correção Espaços em Branco

--- src/com/coddeveloper/JogoForca/TelaControle.java
+++ src/com/coddeveloper/JogoForca/TelaControle.java
@@ -1,14 +1,7 @@
 package com.coddeveloper.JogoForca;
-
 public class TelaControle {
-
 	public static void main(String[] args) {
 		TelaGameOver inicio = new TelaGameOver();
 		inicio.main(null);
-		
-		//compNome ini = new compNome();
-		//ini.main(null);
-
 	}
-
 }
