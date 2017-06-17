92f1fb423bf86ffd3841dd986918cdd634ca0be9
FreeDeveloper <developer@freedeveloper.com.br>
2017-06-17 15:07:33 -0300

    Correção espaços Imports

--- src/com/coddeveloper/JogoForca/compNome.java
+++ src/com/coddeveloper/JogoForca/compNome.java
@@ -1,36 +1,28 @@
 package com.coddeveloper.JogoForca;
-
 import img.imgControl;
-
 public class compNome extends gameForca {
 	private String letra;
 	private static String nomeForca;
 	private static int imgNum = 0;
-	private static int vitoria=0;
+	private static int vitoria = 0;
 	public static void comparaPalavra(String letra) throws Exception {
-		System.out.println("A Letra precionada " + letra);
 		int cont = 0;
 		boolean validaButton = false;
 		char[] chaNomeForca = nomeForca.toCharArray();
-		
 		do {
-			System.out.println(chaNomeForca[cont]);
+
 			if (letra.charAt(0) == chaNomeForca[cont]) {
-				System.out.println("Encontrado esta Letra...> ");
 				validaButton = true;
 				vitoria++;
-				setLabel(cont, letra);}
+				setLabel(cont, letra);
+			}
 			cont++;
 		} while (cont < getNumeroLetras());
-		System.out.println("A quantidade de Letras > "+gameForca.getNumeroLetras());
-		System.out.println("contagem regressiva para vitoria: "+vitoria);
-		if(vitoria==gameForca.getNumeroLetras()){
-			System.out.println("Você venceu ");
+		if (vitoria == gameForca.getNumeroLetras()) {
 			gameForca.setOver(false);
 			gameForca.setVitoriA(true);
 			gameForca.main(null);
-			//TelaGameOver.main(null);
-		}else if(validaButton == false) {
+		} else if (validaButton == false) {
 			imgNum++;
 			imgControl.imgControl(imgNum);
 			gameForca.setBt(false);
@@ -44,10 +36,9 @@ public class compNome extends gameForca {
 		} else if (validaButton == true) {
 			gameForca.setBt(true);
 		}
-		System.out.println("  O resultado da Comparação é: " + getNomeForca());
 	}
 	public static void geraPalavra() {
-	
+
 		nomeRandom palavra = nomeRandom.getRandom();
 		nomeForca = String.valueOf(palavra);
 		int contador = 0;
@@ -74,5 +65,4 @@ public class compNome extends gameForca {
 	public static void setVitoria(int vitoria) {
 		compNome.vitoria = vitoria;
 	}
-	
 }
